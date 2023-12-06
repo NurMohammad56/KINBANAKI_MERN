aaf72d008cf86730d28f46a1fc1ddc288bed1892
Nur Mohammad <milonsardar56@gmail.com>
2023-12-06 18:56:05 +0600

    Cart model completed

---
+++ src/model/CartModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dataSchema = mongoose.Schema(
+    {
+        productID: {type: mongoose.Schema.Types.ObjectId, required:true},
+        userID: {type: mongoose.Schema.Types.ObjectId, required:true},
+        color: {type:String, required:true},
+        price: {type:String, required:true},
+        qty: {type:String, required:true},
+        size: {type:String, required:true},
+    },
+    {timestamps:true, versionKey:false}
+)
+
+const cartModel = mongoose.model("carts", dataSchema);
+module.exports = cartModel;

71c9736e63b1dcbc4360779e4dc73b379e2457bb
Nur Mohammad <milonsardar56@gmail.com>
2023-12-06 21:18:38 +0600

    Invoice model completed

---
+++ src/model/InvoiceModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dataSchema = mongoose.Schema(
+    {
+        userID: {type: mongoose.Schema.Types.ObjectId, required:true},
+        payable: {type:String, required:true},
+        cus_details: {type:String, required:true},
+        ship_details: {type:String, required:true},
+        tran_id: {type:String, required:true},
+        val_id: {type:String, required:true},
+        delivery_status: {type:String, required:true},
+        payment_status: {type:String, required:true},
+        total: {type:String, required:true},
+        vat: {type:String, required:true},
+    },
+    {timestamps:true, versionKey:false}
+)
+
+const invoiceModel = mongoose.model("invoices", dataSchema);
+module.exports = invoiceModel;

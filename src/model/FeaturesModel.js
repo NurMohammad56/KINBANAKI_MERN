aa30fd5d1c7738715602d7bc35fdf52214a5607d
Nur Mohammad <milonsardar56@gmail.com>
2023-12-07 16:12:09 +0600

    Feature model completed

---
+++ src/model/FeaturesModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const dataSchema = mongoose.Schema(
+    {
+        name: {type:String, required:true},
+        des: {type:String, required:true},
+        img: {type:String, required:true},
+
+    },
+    {timestamps:true, versionKey:false}
+)
+
+const featureModel = mongoose.model("features", dataSchema);
+module.exports = featureModel;

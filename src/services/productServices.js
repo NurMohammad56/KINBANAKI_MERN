ae14f595d5787e89207468b40f5d0e5ccd7a112b
Nur Mohammad <milonsardar56@gmail.com>
2023-12-10 18:01:10 +0600

    Services structure completed

---
+++ src/services/productServices.js
@@ -0,0 +1,61 @@
+const brandModel = require("../model/BrandModel");
+const categoryModel = require("../model/CategoryModel");
+const productModel = require("../model/ProductModel");
+const productSliderModel = require("../model/ProductSliderModel");
+const productDetailModel = require("../model/ProductDetailModel");
+const ReviewModel = require("../model/ReviewModel");
+
+
+const brandListService = async ()=>{
+
+}
+
+const categoryListService = async ()=>{
+
+}
+
+const sliderListService = async ()=>{
+
+}
+
+const listByBrandService = async ()=>{
+
+}
+
+const listByCategoryService = async ()=>{
+
+}
+
+const listBySimilarService = async ()=>{
+
+}
+
+const listByKeywordService = async ()=>{
+
+}
+
+const listByRemarkService = async ()=>{
+
+}
+
+const productDetailsService = async ()=>{
+
+}
+
+const productReviewListService = async ()=>{
+
+}
+
+
+module.exports = {
+    brandListService,
+    categoryListService,
+    sliderListService,
+    listByBrandService,
+    listByCategoryService,
+    listBySimilarService,
+    listByKeywordService,
+    listByRemarkService,
+    productDetailsService,
+    productReviewListService
+}

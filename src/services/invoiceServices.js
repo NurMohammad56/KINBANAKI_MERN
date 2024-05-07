cc20d8e454c135a494fc91ae00909e4a933c204a
Nur Mohammad <milonsardar56@gmail.com>
2024-05-07 17:07:13 +0600

    Customer details & Shipping details completed

--- src/services/invoiceServices.js
+++ src/services/invoiceServices.js
@@ -8,122 +8,126 @@ const ObjectID = mongoose.Types.ObjectId;
 const FormData = require("form-data");
 const Axios = require("axios");
 
-const createInvoiceService = async (req) =>{
-    try {
-        let user_id=new ObjectID(req.headers.user_id);
-        let cus_email=req.headers.email;
+const createInvoiceService = async (req) => {
+  try {
+    let user_id = new ObjectID(req.headers.user_id);
+    let cus_email = req.headers.email;
 
-// ============Step 01: Calculate Total Payable & Vat==================================================
-let matchStage={$match:{userID:user_id}}
-let JoinStageProduct={$lookup:{from:"products",localField:"productID",foreignField:"_id",as:"product"}}
-let unwindStage={$unwind:"$product"}
-let cartProducts=await CartModel.aggregate([matchStage,JoinStageProduct,unwindStage]);
+    // ============Step 01: Calculate Total Payable & Vat==================================================
+    let matchStage = { $match: { userID: user_id } };
+    let JoinStageProduct = {
+      $lookup: {
+        from: "products",
+        localField: "productID",
+        foreignField: "_id",
+        as: "product",
+      },
+    };
+    let unwindStage = { $unwind: "$product" };
+    let cartProducts = await CartModel.aggregate([
+      matchStage,
+      JoinStageProduct,
+      unwindStage,
+    ]);
 
-let totalAmount = 0;
-cartProducts.forEach((element) => {
-    let price;
-    if(element['product']['discount']){
-        price = parseFloat(element['product']['discountPrice'])
-    }else{
-        price = parseFloat(element['product']['price']) 
-    }
-    totalAmount += parseFloat(element['qty'])*price;
-});
+    let totalAmount = 0;
+    cartProducts.forEach((element) => {
+      let price;
+      if (element["product"]["discount"]) {
+        price = parseFloat(element["product"]["discountPrice"]);
+      } else {
+        price = parseFloat(element["product"]["price"]);
+      }
+      totalAmount += parseFloat(element["qty"]) * price;
+    });
 
-let vat = totalAmount* 0.05 //5% vat
-let payable = totalAmount+vat
-// ============Step 01: Calculate Payable and Vat==================================================
+    let vat = totalAmount * 0.05; //5% vat
+    let payable = totalAmount + vat;
 
+    // ============Step 02: Prepare Customer Details & Shipping Details==================================================
 
-// ============Step 01: Calculate Payable and Vat==================================================
+    let profile = await ProfileModel.aggregate([matchStage]);
+    let cus_details = `
+     Name:${profile[0]["cus_name"]},
+     Email:${cus_email},
+     Address:${profile[0]["cus_add"]},
+     City:${profile[0]["cus_city"]},
+     Phone:${profile[0]["cus_phone"]},
+    `;
 
+    let shipping_details = `
+     Name:${profile[0]["ship_name"]},
+     City:${profile[0]["ship_city"]},
+     Address:${profile[0]["ship_add"]},
+     State:${profile[0]["ship_state"]},
+     Phone:${profile[0]["ship_phone"]},
+    `;
+    // ============Step 01: Calculate Payable and Vat==================================================
 
-// ============Step 01: Calculate Payable and Vat==================================================
+    // ============Step 01: Calculate Payable and Vat==================================================
 
+    // ============Step 01: Calculate Payable and Vat==================================================
 
-// ============Step 01: Calculate Payable and Vat==================================================
+    // ============Step 01: Calculate Payable and Vat==================================================
 
+    // ============Step 01: Calculate Payable and Vat==================================================
 
-// ============Step 01: Calculate Payable and Vat==================================================
+    // ============Step 01: Calculate Payable and Vat==================================================
 
+    return { status: "Success", data: profile };
+  } catch (error) {
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
-// ============Step 01: Calculate Payable and Vat==================================================
+const paymentFailService = async (req) => {
+  try {
+  } catch (error) {
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
+const paymentCancelService = async (req) => {
+  try {
+  } catch (error) {
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
-// ============Step 01: Calculate Payable and Vat==================================================
+const paymentIPNService = async (req) => {
+  try {
+  } catch (error) {
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
-        return {status:"Success", data:payable}
-    } catch (error) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
-}
+const paymentSuccessService = async (req) => {
+  try {
+  } catch (error) {
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
-const paymentFailService = async (req) =>{
-    try {
-        
+const invoiceListService = async (req) => {
+  try {
+  } catch (error) {
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
-        
-    } catch (error) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
-}
+const invoiceProductListService = async (req) => {
+  try {
+  } catch (error) {
+    return { status: "fail", message: "Something Went Wrong !" };
+  }
+};
 
-const paymentCancelService = async (req) =>{
-    try {
-        
-
-        
-    } catch (error) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
-}
-
-const paymentIPNService = async (req) =>{
-    try {
-        
-
-        
-    } catch (error) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
-}
-
-const paymentSuccessService = async (req) =>{
-    try {
-        
-
-        
-    } catch (error) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
-}
-
-const invoiceListService = async (req) =>{
-    try {
-        
-
-        
-    } catch (error) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
-}
-
-const invoiceProductListService = async (req) =>{
-    try {
-        
-
-        
-    } catch (error) {
-        return {status:"fail",message:"Something Went Wrong !"}
-    }
-}
-
-module.exports={
-    createInvoiceService,
-    paymentCancelService,
-    paymentFailService,
-    paymentIPNService,
-    paymentSuccessService,
-    invoiceListService,
-    invoiceProductListService
-}
+module.exports = {
+  createInvoiceService,
+  paymentCancelService,
+  paymentFailService,
+  paymentIPNService,
+  paymentSuccessService,
+  invoiceListService,
+  invoiceProductListService,
+};

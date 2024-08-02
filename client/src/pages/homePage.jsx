7304fd1edb1f70f411257ea51f347044fdc7067e
Nur Mohammad <milonsardar56@gmail.com>
2024-08-02 19:04:58 +0600

    Categories skeleton animation completed

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Layout from "../component/layout/layout";
 import SliderSkeleton from "../skeleton/slider-skeleton";
 import FeaturesSkeleton from "../skeleton/features-skeleton";
+import CategoriesSkeleton from "../skeleton/categories-skeleton";
 const HomePage = () => {
   return (
     <Layout>
       <SliderSkeleton />
       <FeaturesSkeleton />
+      <CategoriesSkeleton />
     </Layout>
   );
 };
--- client/src/skeleton/features-skeleton.jsx
+++ client/src/skeleton/features-skeleton.jsx
@@ -7,24 +7,28 @@ const FeaturesSkeleton = () => {
   return (
     <div className="container section">
       <div className="row">
-        <div className="col-6 p-2 col-md-3 col-lg-3 col-sm-6">
-          <div className="card shadow-sm">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-4">
-                  <Lottie
-                    className="w-100"
-                    animationData={ImagePlaceholder}
-                    loop={true}
-                  />
-                </div>
-                <div className="col-8">
-                  <Skeleton count={3} />
+        {Array.from({ length: 4 }).map(() => {
+          return (
+            <div className="col-6 p-2 col-md-3 col-lg-3 col-sm-6">
+              <div className="card shadow-sm">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-4">
+                      <Lottie
+                        className="w-100"
+                        animationData={ImagePlaceholder}
+                        loop={true}
+                      />
+                    </div>
+                    <div className="col-8">
+                      <Skeleton count={3} />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );

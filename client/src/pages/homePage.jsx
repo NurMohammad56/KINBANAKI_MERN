6e9ae6ac2f6e7a63cb2bb95cd9ecce72bbce512c
Nur Mohammad <milonsardar56@gmail.com>
2024-08-02 21:48:10 +0600

    Product skeleton animation completed

--- client/src/pages/homePage.jsx
+++ client/src/pages/homePage.jsx
@@ -3,12 +3,14 @@ import Layout from "../component/layout/layout";
 import SliderSkeleton from "../skeleton/slider-skeleton";
 import FeaturesSkeleton from "../skeleton/features-skeleton";
 import CategoriesSkeleton from "../skeleton/categories-skeleton";
+import ProductsSkeleton from "../skeleton/products-skeleton";
 const HomePage = () => {
   return (
     <Layout>
       <SliderSkeleton />
       <FeaturesSkeleton />
       <CategoriesSkeleton />
+      <ProductsSkeleton />
     </Layout>
   );
 };

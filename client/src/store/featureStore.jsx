d449d1b3692b77157b7dd19dbb0f89aabeea2a35
Nur Mohammad <milonsardar56@gmail.com>
2024-08-03 20:58:56 +0600

    Create feature store for calling api

--- client/src/store/featureStore.jsx
+++ client/src/store/featureStore.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import create from "zustand";
+import axios from "axios";
 
-const FeatureStore = () => {
-  return <div></div>;
-};
+const FeatureStore = create((set) => ({
+  FeatureList: null,
+  FeatureListRequest: async () => {
+    let res = await axios.get("/api/v1/featuresList");
+    if (res.data["status"] === "success") {
+      set({ FeatureList: res.data["data"] });
+    }
+  },
+}));
 
 export default FeatureStore;

fce2ff2d5f5eeb7e55b12c5f62e361a66bc6f554
Nur Mohammad <milonsardar56@gmail.com>
2024-08-03 21:07:31 +0600

    Create brand store for call brand apis

--- client/src/store/productStore.jsx
+++ client/src/store/productStore.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import create from "zustand";
+import axios from "axios";
 
-const ProductStore = () => {
-  return <div></div>;
-};
-
+const ProductStore = create((set) => ({
+  BrandList: null,
+  BrandListRequest: async () => {
+    let res = await axios.get("/api/v1/brandList");
+    if (res.data["status"] === "success") {
+      set({ BrandList: res.data["data"] });
+    }
+  },
+}));
 export default ProductStore;

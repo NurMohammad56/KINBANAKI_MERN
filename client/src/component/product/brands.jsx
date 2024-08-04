c7858605db99a38dd7800d3fc181237db5933c10
Nur Mohammad <milonsardar56@gmail.com>
2024-08-04 17:15:14 +0600

    Succesfully render the brand image and brand name to website

--- client/src/component/product/brands.jsx
+++ client/src/component/product/brands.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductStore from "../../store/productStore";
 import BrandsSkeleton from "../../skeleton/brands-skeleton";
+import { Link } from "react-router-dom";
 
 const Brands = () => {
   const { BrandList } = ProductStore();
@@ -8,7 +9,34 @@ const Brands = () => {
   if (BrandList === null) {
     return <BrandsSkeleton />;
   } else {
-    return <div></div>;
+    return (
+      <div className="section">
+        <div className="container">
+          <div className="row">
+            <h1 className="headline-4 text-center my-2 p-0">Top Brands</h1>
+            <span className="bodySmal mb-5 text-center">
+              Explore The Products Across Our Most Popular <br />
+              Shopping Brands{" "}
+            </span>
+            {BrandList.map((item, i) => {
+              return (
+                <div
+                  key={i}
+                  className="col-6 col-lg-8r text-center col-md-8r p-2"
+                >
+                  <Link to="" className="card h-100 rounded-3 bg-white">
+                    <div className="card-body">
+                      <img alt="img" className="w-75" src={item["brandImg"]} />
+                      <p className="bodySmal mt-3">{item["brandName"]}</p>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
   }
 };
 

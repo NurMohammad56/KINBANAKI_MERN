3965ceaceb734ca24e3d88b8650d93de4c3cd5f8
Nur Mohammad <milonsardar56@gmail.com>
2024-08-04 17:50:01 +0600

    Feature details rendering complted

--- client/src/component/features/features.jsx
+++ client/src/component/features/features.jsx
@@ -1,14 +1,38 @@
 import React from "react";
-import ProductStore from "../../store/productStore";
 import FeaturesSkeleton from "./../../skeleton/features-skeleton";
+import FeatureStore from "../../store/featureStore";
 
 const Features = () => {
-  const { FeatureList } = ProductStore();
+  const { FeatureList } = FeatureStore();
 
   if (FeatureList === null) {
     return <FeaturesSkeleton />;
   } else {
-    return <div></div>;
+    return (
+      <div className="container section">
+        <div className="row">
+          {FeatureList.map((item, i) => {
+            return (
+              <div key={i} className="col-6 p-2 col-md-3 col-lg-3 col-sm-6">
+                <div className="card shadow-sm">
+                  <div className="card-body">
+                    <div className="row">
+                      <div className="col-3">
+                        <img alt="img" className="w-100" src={item["img"]} />
+                      </div>
+                      <div className="col-9">
+                        <h3 className="bodyXLarge">{item["name"]}</h3>
+                        <span className="bodySmal">{item["description"]}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 };
 
--- client/src/component/product/brands.jsx
+++ client/src/component/product/brands.jsx
@@ -24,7 +24,10 @@ const Brands = () => {
                   key={i}
                   className="col-6 col-lg-8r text-center col-md-8r p-2"
                 >
-                  <Link to="" className="card h-100 rounded-3 bg-white">
+                  <Link
+                    to={`/listByBrand/${item["_id"]}`}
+                    className="card h-100 rounded-3 bg-white"
+                  >
                     <div className="card-body">
                       <img alt="img" className="w-75" src={item["brandImg"]} />
                       <p className="bodySmal mt-3">{item["brandName"]}</p>

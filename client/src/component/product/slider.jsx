4541044f788663a3b8300f347fb8f4a312779790
Nur Mohammad <milonsardar56@gmail.com>
2024-08-05 02:18:48 +0600

    Create a new slider carousel and solving the sliding problem

--- client/src/component/product/slider.jsx
+++ client/src/component/product/slider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProductStore from "../../store/productStore.js";
 import SliderSkeleton from "./../../skeleton/slider-skeleton";
 import { Link } from "react-router-dom";
+import { Carousel } from "react-bootstrap";
 
 const Slider = () => {
   const { SliderList } = ProductStore();
@@ -10,34 +11,12 @@ const Slider = () => {
     return <SliderSkeleton />;
   } else {
     return (
-      <div>
-        <div
-          id="carouselExampleDark"
-          className="carousel hero-bg carousel-dark slide"
-        >
-          <div className="carousel-indicators">
-            {SliderList.map((i) => {
-              return (
-                <button
-                  key={i}
-                  type="button"
-                  data-bs-target="#carouselExampleDark"
-                  data-bs-slide-to={i}
-                  className="active"
-                  aria-current="true"
-                  aria-label=""
-                ></button>
-              );
-            })}
-          </div>
-          <div className="carousel-inner py-5">
+      <div className="carousel hero-bg carousel-dark slide">
+        <div className="carousel-inner py-5">
+          <Carousel>
             {SliderList.map((item, i) => {
-              let active = "carousel-item";
-              if (i === 0) {
-                active = "carousel-item active";
-              }
               return (
-                <div key={i} className={active} data-bs-interval="10000">
+                <Carousel.Item key={i}>
                   <div className="container">
                     <div className="row justify-content-center">
                       <div className="col-12 col-lg-5 col-sm-12 col-md-5 p-5">
@@ -48,38 +27,14 @@ const Slider = () => {
                         </Link>
                       </div>
                       <div className="col-12 col-lg-5 col-sm-12 col-md-5 p-5">
-                        <img src={item["image"]} className="w-100" alt="..." />
+                        <img className="w-100" src={item["image"]} alt="..." />
                       </div>
                     </div>
                   </div>
-                </div>
+                </Carousel.Item>
               );
             })}
-          </div>
-          <button
-            className="carousel-control-prev btn rounded-5"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next btn"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+          </Carousel>
         </div>
       </div>
     );

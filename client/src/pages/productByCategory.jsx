cf18856f4683fd0b2c9fba4c27da9b7a137d131d
Nur Mohammad <milonsardar56@gmail.com>
2024-08-07 20:35:43 +0600

    ProductList by category completed

---
+++ client/src/pages/productByCategory.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import ProductStore from "../store/productStore";
+import { useParams } from "react-router-dom";
+import Layout from "../component/layout/layout";
+import ProductList from "../component/product/productList";
+
+const ProductByCategory = () => {
+  const { ListByCategoryRequest } = ProductStore();
+  const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      await ListByCategoryRequest(id);
+    })();
+  }, []);
+  return (
+    <Layout>
+      <ProductList />
+    </Layout>
+  );
+};
+
+export default ProductByCategory;

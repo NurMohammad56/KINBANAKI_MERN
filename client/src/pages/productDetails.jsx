39da6d9aeb0288fee6cb3bdd26ceae0d1c0cc59d
Nur Mohammad <milonsardar56@gmail.com>
2024-08-10 22:03:34 +0600

    Details frontend completed

--- client/src/pages/productDetails.jsx
+++ client/src/pages/productDetails.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Details from "../component/product/details";
 import Layout from "../component/layout/layout";
 import Brands from "./../component/product/brands";
+import { useParams } from "react-router-dom";
+import ProductStore from "../store/productStore";
 
 const ProductDetails = () => {
+  const {
+    ReviewListRequest,
+    ProductDetailsRequest,
+    BrandList,
+    ListByBrandRequest,
+  } = ProductStore();
+  const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      await ProductDetailsRequest(id);
+      await ReviewListRequest(id);
+      BrandList === null ? await ListByBrandRequest() : null;
+    })();
+  }, []);
+
   return (
     <Layout>
       <Details />

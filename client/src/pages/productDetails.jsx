a9fdb54b1821f263006fc4f4137df0710fa9e5d9
Nur Mohammad <milonsardar56@gmail.com>
2024-08-10 21:37:18 +0600

    Render brands component in product details page

--- client/src/pages/productDetails.jsx
+++ client/src/pages/productDetails.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Details from "../component/product/details";
 import Layout from "../component/layout/layout";
+import Brands from "./../component/product/brands";
 
 const ProductDetails = () => {
   return (
     <Layout>
       <Details />
+      <Brands />
     </Layout>
   );
 };

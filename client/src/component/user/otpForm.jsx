1125cd6c1443a46258f90f2616760e417d86a901
Nur Mohammad <milonsardar56@gmail.com>
2024-08-14 22:00:47 +0600

    Create markup for otpForm

--- client/src/component/user/otpForm.jsx
+++ client/src/component/user/otpForm.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import SubmitButton from "../layout/submitButton";
-
-const LoginForm = () => {
+const OtpForm = () => {
   return (
     <div className="container section">
       <div className="row d-flex justify-content-center">
         <div className="col-md-5">
           <div className="card p-5">
-            <h4>Enter Your Email</h4>
+            <h4>Enter Verification Code</h4>
             <p>
-              A verification code will be sent to the email address you provide
+              A verification code will be sent the email address you provide
             </p>
             <input
               type="email"
@@ -18,7 +17,7 @@ const LoginForm = () => {
             />
             <SubmitButton
               submit={false}
-              className="btn mt-3 btn-success"
+              className="btn btn-success mt-3"
               text="Next"
             />
           </div>
@@ -27,5 +26,4 @@ const LoginForm = () => {
     </div>
   );
 };
-
-export default LoginForm;
+export default OtpForm;

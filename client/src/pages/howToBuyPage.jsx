93cbd0d9a19a4e1fabb9f6d35fc9b0494fdca981
Nur Mohammad <milonsardar56@gmail.com>
2024-08-14 19:36:19 +0600

    HowToBuy page completed

--- client/src/pages/howToBuyPage.jsx
+++ client/src/pages/howToBuyPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import Layout from "./../component/layout/layout";
+import LegalContents from "../component/features/legalContents";
+import FeatureStore from "../store/featureStore";
 const HowToBuyPage = () => {
-  return <div></div>;
+  const { LegalDetailsRequest } = FeatureStore();
+
+  useEffect(() => {
+    (async () => {
+      await LegalDetailsRequest("howToBuy");
+    })();
+  }, []);
+
+  return (
+    <Layout>
+      <LegalContents />
+    </Layout>
+  );
 };
 
 export default HowToBuyPage;

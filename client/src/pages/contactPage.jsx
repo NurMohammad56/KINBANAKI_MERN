b1580ad7cbaf649fd47e75b29dac289a756f9c92
Nur Mohammad <milonsardar56@gmail.com>
2024-08-14 19:34:42 +0600

    Contact page completed

--- client/src/pages/contactPage.jsx
+++ client/src/pages/contactPage.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import Layout from "./../component/layout/layout";
+import LegalContents from "../component/features/legalContents";
+import FeatureStore from "../store/featureStore";
 const ContactPage = () => {
-  return <div></div>;
+  const { LegalDetailsRequest } = FeatureStore();
+
+  useEffect(() => {
+    (async () => {
+      await LegalDetailsRequest("contact");
+    })();
+  }, []);
+
+  return (
+    <Layout>
+      <LegalContents />
+    </Layout>
+  );
 };
 
 export default ContactPage;

d3e9c5dafe7e033c9292f066547c331ba5c921e2
Nur Mohammad <milonsardar56@gmail.com>
2024-08-26 21:41:45 +0600

    CartForm completed

--- client/src/store/cartStore.js
+++ client/src/store/cartStore.js
@@ -1,8 +1,18 @@
 import { create } from "zustand";
 import axios from "axios";
+import { unauthorized } from "./../utility/utility";
 
 const CartStore = create((set) => ({
   isCartSubmit: false,
+  CartForm: { productID: "", color: "1", qty: "", size: "" },
+  CartFormChange: (name, value) => {
+    set((state) => ({
+      CartForm: {
+        ...state.CartForm,
+        [name]: value,
+      },
+    }));
+  },
 }));
 
 export default CartStore;

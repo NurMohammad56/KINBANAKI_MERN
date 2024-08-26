93c238f9d2777d9fa59d7e7e17bde9e9f9b17b17
Nur Mohammad <milonsardar56@gmail.com>
2024-08-26 21:53:10 +0600

    CartList Api calling completed

--- client/src/store/cartStore.js
+++ client/src/store/cartStore.js
@@ -13,6 +13,31 @@ const CartStore = create((set) => ({
       },
     }));
   },
+
+  CartSaveRequest: async (PostBody, productID) => {
+    try {
+      set({ isCartSubmit: true });
+      PostBody.productID = productID;
+      let res = await axios.post(`/api/v1/saveCartList`, PostBody);
+      return res.data["status"] === "Success";
+    } catch (error) {
+      unauthorized(error.response.status);
+    } finally {
+      set({ isCartSubmit: false });
+    }
+  },
+
+  CartList: null,
+  CartCount: 0,
+  CartListRequest: async () => {
+    try {
+      let res = await axios.get(`/api/v1/cartList`);
+      set({ CartList: res.data["data"] });
+      set({ CartCount: res.data["data"].length });
+    } catch (error) {
+      unauthorized(error.response.status);
+    }
+  },
 }));
 
 export default CartStore;

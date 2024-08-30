a3fdfa37eeb39ea91e753378f532f6a5c448080f
Nur Mohammad <milonsardar56@gmail.com>
2024-08-30 23:46:49 +0600

    Wish api calling completed

--- client/src/store/wishStore.js
+++ client/src/store/wishStore.js
@@ -2,6 +2,19 @@ import React from "react";
 
 const WishStore = () => {
   isWistSubmit: false;
+  WishSaveRequest: async (productID) => {
+    try {
+      set({ isWistSubmit: true });
+      let res = await axios.post(`/api/v1/saveWishList`, {
+        productID: productID,
+      });
+      return res.data["status"] === "Success";
+    } catch (error) {
+      unauthorized(error.response.status);
+    } finally {
+      set({ isWistSubmit: false });
+    }
+  };
 };
 
 export default WishStore;

71ffd6ce7aa8a5252571244782635de95844c81d
Nur Mohammad <milonsardar56@gmail.com>
2024-09-09 00:12:58 +0600

    Some changes on invoice controller

--- src/controller/invoiceController.js
+++ src/controller/invoiceController.js
@@ -1,44 +1,44 @@
 const {
-    createInvoiceService,
-    paymentCancelService,
-    paymentFailService,
-    paymentIPNService,
-    paymentSuccessService,
-    invoiceListService,
-    invoiceProductListService
+  createInvoiceService,
+  paymentCancelService,
+  paymentFailService,
+  paymentIPNService,
+  paymentSuccessService,
+  invoiceListService,
+  invoiceProductListService,
 } = require("../services/invoiceServices");
 
-exports.createInvoice = async (req, res)=>{
-    let result = await createInvoiceService(req);
-    return res.status(200).json(result);
-}
+exports.createInvoice = async (req, res) => {
+  let result = await createInvoiceService(req);
+  return res.status(200).json(result);
+};
 
-exports.paymentCancel = async (req, res)=>{
-    let result = await paymentCancelService(req);
-    return res.status(200).json(result);
-}
+exports.paymentCancel = async (req, res) => {
+  let result = await paymentCancelService(req);
+  return res.redirect("/orders");
+};
 
-exports.paymentFail = async (req, res)=>{
-    let result = await paymentFailService(req);
-    return res.status(200).json(result);
-}
+exports.paymentFail = async (req, res) => {
+  let result = await paymentFailService(req);
+  return res.redirect("/orders");
+};
 
-exports.paymentIPN = async (req, res)=>{
-    let result = await paymentIPNService(req);
-    return res.status(200).json(result);
-}
+exports.paymentIPN = async (req, res) => {
+  let result = await paymentIPNService(req);
+  return res.redirect("/orders");
+};
 
-exports.paymentSuccess = async (req, res)=>{
-    let result = await paymentSuccessService(req);
-    return res.status(200).json(result);
-}
+exports.paymentSuccess = async (req, res) => {
+  let result = await paymentSuccessService(req);
+  return res.redirect("/orders");
+};
 
-exports.invoiceList= async (req, res)=>{
-    let result = await invoiceListService(req);
-    return res.status(200).json(result);
-}
+exports.invoiceList = async (req, res) => {
+  let result = await invoiceListService(req);
+  return res.status(200).json(result);
+};
 
-exports.invoiceProductList= async (req, res)=>{
-    let result = await invoiceProductListService(req);
-    return res.status(200).json(result);
-}
+exports.invoiceProductList = async (req, res) => {
+  let result = await invoiceProductListService(req);
+  return res.status(200).json(result);
+};
